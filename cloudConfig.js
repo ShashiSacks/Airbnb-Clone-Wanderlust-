90dc270a2b608d229dafc1a6237ed2082d90b9ca
ShashiSacks <shashidharreddy.mopuri@gmail.com>
2026-05-27 22:50:46 +0530

    Fixed cloudinary dependency compatibility

--- cloudConfig.js
+++ cloudConfig.js
@@ -1,42 +1,33 @@
 const cloudinary = require("cloudinary").v2;
 
 const {
-  storage: CloudinaryStorage,
+  CloudinaryStorage,
 } = require("multer-storage-cloudinary");
 
 cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
 
-  cloud_name:
-    process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
 
-  api_key:
-    process.env.CLOUD_API_KEY,
-
-  api_secret:
-    process.env.CLOUD_API_SECRET,
+  api_secret: process.env.CLOUD_API_SECRET,
 });
 
-const storage =
-  new CloudinaryStorage({
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
 
-    cloudinary,
+  params: {
+    folder: "wanderlust_DEV",
 
-    params: {
-
-      folder: "wanderlust_DEV",
-
-      allowed_formats: [
-        "png",
-        "jpg",
-        "jpeg",
-        "webp",
-      ],
-    },
-  });
+    allowed_formats: [
+      "png",
+      "jpg",
+      "jpeg",
+      "webp",
+    ],
+  },
+});
 
 module.exports = {
-
   cloudinary,
-
   storage,
 };
